6f348b0aa2bdfc9c836e465e17bf61ad3fbbe254
zoombooze <zakyahmad0023@gmail.com>
2024-02-12 23:29:58 +0700

    chore: add some routing for client side

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import NavbarComponent from './components/Navbar.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavbarComponent />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
