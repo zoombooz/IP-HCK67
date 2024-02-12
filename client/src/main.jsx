5e531742c7d83ceaecebcfeb6a9acde07d60643d
zoombooze <zakyahmad0023@gmail.com>
2024-02-12 23:39:34 +0700

    chore: add some path to routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import NavbarComponent from './components/Navbar.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <NavbarComponent />
+    element: 
+    <>
+      <h1>Home Page</h1>
+    </>
+  },
+  {
+    path: "/product",
+    element: 
+    <>
+      <h1>Product List Page</h1>
+    </>
+  },
+  {
+    path: "/product/:id",
+    element: 
+    <>
+      <h1>Product Detail Page</h1>
+    </>
+  },
+  {
+    path: "/cart",
+    element: 
+    <>
+      <h1>User Cart Page</h1>
+    </>
+  },
+  {
+    path: "/product-list",
+    element: 
+    <>
+      <h1>Product CMS Page</h1>
+    </>
+  },
+  {
+    path: "/product-list/add",
+    element: 
+    <>
+      <h1>Product Add Page</h1>
+    </>
+  },
+  {
+    path: "/product-list/update",
+    element: 
+    <>
+      <h1>Product Update Page</h1>
+    </>
   },
 ]);
 
